ccef7da034c5aeced32487d6094dd3952b870685
Reuben George <reuben_george1998@outlook.com>
2022-01-14 12:34:16 +0000

    getByID and deleteByID added

--- src/main/java/com/example/readinglist/ReadingListController.java
+++ src/main/java/com/example/readinglist/ReadingListController.java
@@ -1,11 +1,9 @@
 package com.example.readinglist;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 import javax.transaction.Transactional;
@@ -27,6 +25,17 @@ public class ReadingListController {
     public ResponseEntity<String> addToReadingList(@RequestBody Book book) {
         repository.save(book);
         return ResponseEntity.status(HttpStatus.CREATED).body("Book added: " + book.getBookName());
+    }
+
+    @GetMapping("/reading-list/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable String id) {
+        return ResponseEntity.status(HttpStatus.OK).body(repository.findBookByid(Integer.parseInt(id)));
+    }
+
+    @DeleteMapping("/reading-list/{id}")
+    public ResponseEntity<String> deleteBook(@PathVariable String id) {
+        repository.deleteBookByid(Integer.parseInt(id));
+        return ResponseEntity.status(HttpStatus.OK).body("Book with ID: " + id + "has been deleted");
     }
 
 }
--- src/main/java/com/example/readinglist/ReadingListRepository.java
+++ src/main/java/com/example/readinglist/ReadingListRepository.java
@@ -5,5 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReadingListRepository extends JpaRepository<Book, String> {
-
+    Book findBookByid(int id);
+    String deleteBookByid(int id);
 }
